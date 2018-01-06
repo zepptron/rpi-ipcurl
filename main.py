d23e6a6f01256dba954fef73053bcb8513c4123d
Sebastian Tornow <sebastian@p7s1produktion.de>
2018-01-06 10:26:48 +0100

    bs

--- main.py
+++ main.py
@@ -17,12 +17,12 @@ while True:
         time.sleep(300) # sleep for 5 minutes before checking again
         ip = load(urllib2.urlopen('https://api.ipify.org/?format=json'))['ip']
     else:
+        ip = load(urllib2.urlopen('https://api.ipify.org/?format=json'))['ip']
+        my_ip = load(urllib2.urlopen('https://api.ipify.org/?format=json'))['ip']
         url_at = "http://dynamicdns.park-your-domain.com/update?host=%s&domain=%s&password=%s&ip=%s" % (os.environ["host"], os.environ["domain"], os.environ["pass"], my_ip)
         url_wc = "http://dynamicdns.park-your-domain.com/update?host=%s&domain=%s&password=%s&ip=%s" % (os.environ["host2"], os.environ["domain"], os.environ["pass"], my_ip)
         urllib2.urlopen(url_at).read()  # update namecheap no.1
         urllib2.urlopen(url_wc).read()  # update namecheap no.2
-        ip = load(urllib2.urlopen('https://api.ipify.org/?format=json'))['ip']
-        my_ip = load(urllib2.urlopen('https://api.ipify.org/?format=json'))['ip']
         ti = time.time()
         ts = datetime.datetime.fromtimestamp(ti).strftime('%d-%m-%Y %H:%M:%S')
         print "%s IP changed to %s" % (ts, my_ip)   # timestamp + infolog
